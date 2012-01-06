62d45563d867d67dcd452aa5e8416ebfb28d0bd3
Eric Hartmann <eric.hartmann@mni.thm.de>
2012-01-06 19:04:53 +0100

    Init-Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klausur::Application.config.secret_token = '9c24c122c0bef8cbc8861c07c07dfad0e47ec83fbc53dc8d2015c36bf9bc6f7f5d48776c6cdcfcc9ce849fa105b4bd7450f5d1232f5cfdf98e6a37c071525d40'
