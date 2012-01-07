e3467c6a79c0ade13f7db85726e20162f2598d19
Eric Hartmann <eric.hartmann@mni.thm.de>
2012-01-07 20:50:24 +0100

    geht noch nich, is aber nich wichtig

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,8 @@
 class Document < ActiveRecord::Base
+  #attr_accessible :id, :name, :file, :remote_file_url
   belongs_to :lesson
   belongs_to :semester
   belongs_to :prof
   belongs_to :doc_type
+  mount_uploader :file, FileUploader
 end
--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,7 +1,11 @@
 # encoding: utf-8
+require 'carrierwave/processing/mime_types'
+
 
 class FileUploader < CarrierWave::Uploader::Base
-
+  
+  include CarrierWave::MimeTypes
+  
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -13,7 +17,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -45,4 +49,6 @@ class FileUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-end
+  process :set_content_type
+
+end  
